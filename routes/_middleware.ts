66dfe65b626c3cf0fc4cb895452fbe8e875de03e
mscno <msc@parap.ly>
2024-01-30 20:34:02 +0100

    add logger

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -1,6 +1,14 @@
 import { FreshContext } from "$fresh/server.ts";
+import Logger from "https://deno.land/x/logger@v1.1.4/logger.ts";
 
-export async function handler(req: Request, ctx: FreshContext) {
+const logger = new Logger();
+
+export const handler = [
+  loggermw,
+  cors,
+];
+
+export async function cors(req: Request, ctx: FreshContext) {
   const origin = req.headers.get("Origin") || "*";
   const resp = await ctx.next();
   const headers = resp.headers;
@@ -16,5 +24,14 @@ export async function handler(req: Request, ctx: FreshContext) {
     "POST, OPTIONS, GET, PUT, DELETE",
   );
 
+  return resp;
+}
+
+export async function loggermw(req: Request, ctx: FreshContext) {
+  const start = Date.now();
+  const resp = await ctx.next();
+  const ms = Date.now() - start;
+  const ts = new Date().toISOString();
+  logger.info(`[${ts}] ${req.method} ${req.url} - ${ms}ms`);
   return resp;
 }
