e31ca8230a13cbb3de427eed63dfb3b72a1fb851
mscno <msc@parap.ly>
2024-01-30 20:38:56 +0100

    fix mw again

--- routes/_middleware.ts
+++ routes/_middleware.ts
@@ -31,7 +31,6 @@ export async function loggermw(req: Request, ctx: FreshContext) {
   const start = Date.now();
   const resp = await ctx.next();
   const ms = Date.now() - start;
-  const ts = new Date().toISOString();
   logger.info(`${req.method} ${req.url} - ${ms}ms`);
   return resp;
 }
